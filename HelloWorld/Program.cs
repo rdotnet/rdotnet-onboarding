a1612b5b00aa19435a6b801ac17d5791668dca5f
J-M <jean-michel.perraud@csiro.au>
2013-10-21 20:47:36 +1100

    update, partly to feed the Documentation page of the R.NET web site

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RDotNet;
+using RDotNet.NativeLibrary;
 using RDotNetSetup;
 using System;
 using System.Collections.Generic;
@@ -13,18 +14,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            SetupHelper.SetupPath();
+            REngine.SetEnvironmentVariables(); // Currently under development - coming soon
             using (REngine engine = REngine.CreateInstance("RDotNet"))
             {
-                // From v1.5, REngine requires explicit initialization.
-                // You can set some parameters.
-                engine.Initialize();
-
-                CharacterVector charVec = engine.CreateCharacterVector(new[]{"Hello, R world!, .NET speaking"});
-
+                engine.Initialize(); // required since v1.5
+                CharacterVector charVec = engine.CreateCharacterVector(new[] { "Hello, R world!, .NET speaking" });
                 engine.SetSymbol("greetings", charVec);
                 engine.Evaluate("str(greetings)"); // print out in the console
-                string[] a = engine.Evaluate("'Hi there .NET, from the R engine'").AsCharacter().ToArray() ;
+                string[] a = engine.Evaluate("'Hi there .NET, from the R engine'").AsCharacter().ToArray();
                 Console.WriteLine("R answered: '{0}'", a[0]);
                 Console.WriteLine("Press any key to exit the program");
                 Console.ReadKey();
