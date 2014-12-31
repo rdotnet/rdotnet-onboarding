c009247626da0d069b2c9ad791798ad81beb874b
J-M <per202@csiro.au>
2014-12-31 18:01:22 +1100

    Add code for a stackoverflow question

--- SupportSamples/Program.cs
+++ SupportSamples/Program.cs
@@ -7,17 +7,20 @@ namespace SupportSamples
 	{
 		public static void Main (string[] args)
 		{
-			stackoverflow_27689786_2752565 ();
+			REngine.SetEnvironmentVariables();
+			REngine engine = REngine.GetInstance();
+			//stackoverflow_27689786_2752565 (engine);
+			stackoverflow_27597542_2752565 (engine);
+			// you should always dispose of the REngine properly.
+			// After disposing of the engine, you cannot reinitialize nor reuse it
+			engine.Dispose();
 		}
 
 		/// <summary>
 		/// http://stackoverflow.com/q/27689786/2752565
 		/// </summary>
-		static void stackoverflow_27689786_2752565 ()
+		static void stackoverflow_27689786_2752565 (REngine engine)
 		{
-			REngine.SetEnvironmentVariables();
-			REngine engine = REngine.GetInstance();
-
 			var rand = new System.Random (0);
 			double[] randValues;
 
@@ -25,9 +28,6 @@ namespace SupportSamples
 				randValues = mkValues (rand, 100);
 				Console.WriteLine ("std dev iteration {0} = {1}", i + 1, calcSDev (engine, randValues));
 			}
-			// you should always dispose of the REngine properly.
-			// After disposing of the engine, you cannot reinitialize nor reuse it
-			engine.Dispose();
 		}
 
 		static double[] mkValues (Random rand, int n)
@@ -46,6 +46,26 @@ namespace SupportSamples
 			NumericVector rVector = engine.CreateNumericVector(arr);
 			engine.SetSymbol ("x", rVector);
 			return engine.Evaluate ("sd(x)").AsNumeric () [0];
+		}
+
+		/// <summary>
+		/// http://stackoverflow.com/q/27597542/2752565
+		/// </summary>
+		static void stackoverflow_27597542_2752565 (REngine engine)
+		{
+			var createModel = @"
+			set.seed(0)
+			x <- ts(rnorm(100))
+			library(forecast)
+			blah <- ets(x)
+			# str(blah)
+			";
+			engine.Evaluate (createModel);
+			var m = engine.GetSymbol ("blah").AsList ();
+			var components = m ["components"].AsCharacter ().ToArray ();
+			for (int i = 0; i < components.Length; i++) {
+				Console.WriteLine ("m$components[{0}] = {1}", i + 1, components [i]);
+			}
 		}
 	}
 }
